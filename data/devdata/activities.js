79f80394328fe9fa6bcf5d400c8aba1e56ebb404
CalStott <caleb.stott@outlook.com>
2025-01-17 12:03:20 +0000

    adjusted activities query

--- data/devdata/activities.js
+++ data/devdata/activities.js
@@ -2,74 +2,76 @@ export default [
 	{
 		title: 'meditation',
 		description: 'meditate with us be calm',
-		duration: '600', //seconds
+		duration: 600, //seconds
 		audioURL: '',
 		imageURL: '',
 		category: 'breathing',
 		difficulty: 'advanced',
-		moodTag: 'stressed'
+		moodTag: 'stressed',
 	},
 
 	{
 		title: 'single tasking',
 		description: 'focus on one thing at a time',
-		duration: '600', //seconds
+		duration: 600, //seconds
 		audioURL: '',
 		imageURL: '',
 		category: 'mindfulness',
 		difficulty: 'beginner',
-		moodTag: 'anxious'
+		moodTag: 'anxious',
 	},
 
 	{
 		title: 'yoga flow',
 		description: 'move your body and ground yourself with yoga',
-		duration: '350', //seconds
+		duration: 350, //seconds
 		audioURL: '',
 		imageURL: '',
 		category: 'movement',
 		difficulty: 'intermediate',
-		moodTag: ['happy', 'anxious']
+		moodTag: ['happy', 'anxious'],
 	},
 
 	{
 		title: 'express gratitude',
 		description: 'journal or speak out loud and be grateful',
+		duration: 120,
 		audioURL: '',
 		imageURL: '',
 		category: 'movement',
 		difficulty: 'intermediate',
-		moodTag: 'happy'
+		moodTag: 'happy',
 	},
 
 	{
 		title: 'reach out to a friend',
 		description: 'make plans or phone a friend today',
+		duration: 300,
 		audioURL: '',
 		imageURL: '',
 		category: 'social connections',
-		moodTag: 'happy'
+		moodTag: 'happy',
 	},
 
 	{
 		title: 'dance party',
 		description: 'some good tunes and move your body',
-		duration: '300', //seconds
+		duration: 300, //seconds
 		audioURL: '',
 		imageURL: '',
 		category: 'movement',
 		difficulty: 'intermediate',
-		moodTag: 'happy'
+		moodTag: 'happy',
 	},
 
 	{
 		title: 'cook your favourite meal!',
 		description: 'treat yourself to some good food',
-		duration: '1200', //seconds
+		duration: 1200, //seconds
 		audioURL: '',
 		imageURL: '',
 		category: 'self-care',
 		difficulty: 'intermediate',
-		moodTag: 'happy'
+		moodTag: 'happy',
 	},
 ];
