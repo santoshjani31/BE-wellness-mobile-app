603db848afcd1afbb3fc2f343f43882e0346f155
CalStott <caleb.stott@outlook.com>
2025-01-20 11:43:01 +0000

    fixed data imports

--- data/testdata/index.js
+++ data/testdata/index.js
@@ -2,10 +2,12 @@ import activitiesData from './activities.js';
 import moodData from './moods.js';
 import userData from './users.js';
 import articlesData from './articles.js';
+import journalEntriesData from './journalentries.js';
 
 export default {
 	activitiesData,
 	moodData,
 	userData,
 	articlesData,
+	journalEntriesData,
 };
